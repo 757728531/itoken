41eefd02fce10b57937afbc9e465c66458d541cb
xuguangxin <757728531@qq.com>
2019-01-30 03:12:33 +0800

    itoken

--- itoken-config/src/main/java/com/funtl/itoken/config/ConfigApplication.java
+++ itoken-config/src/main/java/com/funtl/itoken/config/ConfigApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class,args);
